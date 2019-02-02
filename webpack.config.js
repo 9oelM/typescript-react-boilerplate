bb1ab1912b86496638dcdf8287691f6cf000ef69
Joel M <hj923@hotmail.com>
2019-02-02 16:51:12 +0000

    let webpack dev server work

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,10 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, "public"),
     compress: true,
-    port: 8080
+    port: 8080,
+    host: "0.0.0.0",
+    publicPath: "/",
+    watchContentBase: true,
+    disableHostCheck: true // for cloud dev environment
   }
 };
